5a473847011e3cdea59ebb068247c9bcf699c9fe
Jyben <benjamin.letessier@gmail.com>
2017-09-05 23:18:35 +0200

    Refactoring background

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -2,25 +2,17 @@ var isNotified = false;
 window.TwitchAlert = new TwitchAlert();
 
 setInterval(function(){
-	TwitchAlert.isOnAir(updateStatus);
+	TwitchAlert.isOnAir(function(isOnAir, context) {
+		if (isOnAir && !isNotified)
+		{
+			TwitchAlert.pushNotification(context.title, TwitchAlert.message);
+			TwitchAlert.setIcon("on");
+		}
+		else if (!isOnAir)
+		{
+			TwitchAlert.setIcon("off");
+		}
+
+		isNotified = isOnAir;
+	});
 }, TwitchAlert.tickRate);
-
-function updateStatus(isOnAir, context)
-{
-	if (isOnAir && !isNotified)
-	{
-		TwitchAlert.pushNotification(context.title, TwitchAlert.message);
-		TwitchAlert.setIcon("on");
-	}
-	else if (!isOnAir)
-	{
-		TwitchAlert.setIcon("off");
-	}
-
-	if (isOnAir)
-	{
-		// Save data
-	}
-
-	isNotified = isOnAir;
-}
