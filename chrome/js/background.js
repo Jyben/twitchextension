ecca2d062c8338651bb197b28960bd6bb0413379
Unknown <benjamin.letessier@capgemini.com>
2018-01-29 22:03:03 +0100

    1.0.3 - Fix notification spam

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,18 +1,21 @@
-var isNotified = false;
+chrome.storage.local.set({'live' : false});
 window.TwitchAlert = new TwitchAlert();
 
-setInterval(function(){
-	TwitchAlert.isOnAir(function(isOnAir, context) {
-		if (isOnAir && !isNotified)
-		{
-			TwitchAlert.pushNotification(context.title, TwitchAlert.message);
-			TwitchAlert.setIcon("on");
-		}
-		else if (!isOnAir)
-		{
-			TwitchAlert.setIcon("off");
-		}
-
-		isNotified = isOnAir;
-	});
-}, TwitchAlert.tickRate);
+chrome.storage.local.get(['live'],function(res)
+{
+	setInterval(function(){
+		TwitchAlert.isOnAir(function(isOnAir, context) {
+			if (isOnAir && !res.live)
+			{
+				TwitchAlert.pushNotification(TwitchAlert.message, context.title);
+				TwitchAlert.setIcon("on");
+				res.live = true;
+			}
+			else if (!isOnAir)
+			{
+				TwitchAlert.setIcon("off");
+				res.live = false;
+			}
+		});
+	}, TwitchAlert.tickRate);
+});
